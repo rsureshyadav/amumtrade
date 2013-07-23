7d8256c66305fac76868381f68ac09950c90a0a0
rsureshyadav@gmail.com <rsureshyadav@gmail.com@110281c2-ae3b-db2f-c1e3-5f523cf22f73>
2013-07-23 19:03:04 +0000

    added amumtrade1.1 changes

--- branches/AMUMTrade1.1/src/com/amumtrade/dao/AMUMStockDAO.java
+++ branches/AMUMTrade1.1/src/com/amumtrade/dao/AMUMStockDAO.java
@@ -2,12 +2,15 @@ package com.amumtrade.dao;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.MathContext;
 import java.net.URL;
-import java.net.URLConnection;
+
+import sun.net.www.protocol.http.HttpURLConnection;
 
 import com.amumtrade.bean.AMUMStockBean;
-import com.amumtrade.helper.AMUMStockWriter;
 
 public class AMUMStockDAO implements Runnable {
 
@@ -20,37 +23,47 @@ public class AMUMStockDAO implements Runnable {
 	
 	@Override
 	public void run() {
+		URL url;
+	    HttpURLConnection connection = null;  
 		try {
-			String charset = "UTF-8";
-			URLConnection connection = new URL("http://finance.yahoo.com/q/ks?s="+stockBean.getSymbol().trim()+"+Key+Statistics").openConnection();
-			connection.setDoOutput(true); // Triggers POST.
-			connection.setRequestProperty("Accept-Charset", charset);
-			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=" + charset);
-			BufferedReader reader = null;
-			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			
-			for (String line; (line = reader.readLine()) != null;) {
-			//	findEPS(line);
-				findDilutedEPS(line);
+			String targetURL = "http://finance.yahoo.com/q/ks?s="+stockBean.getSymbol().trim()+"+Key+Statistics";
+			 url = new URL(targetURL);
+		      connection = (HttpURLConnection)url.openConnection();
+		      connection.setReadTimeout(5000);
+		      
+		      
+		      InputStream is = connection.getInputStream();
+		      BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		      String line;
+		      while((line = br.readLine()) != null) {
+		        findDilutedEPS(line);
 				findOperatingMargin(line);
 				findReturnOnAssets(line);
 				findReturnOnEquity(line);
 				findRevenuePerShare(line);
-	        }
-			
+				
+		      }
+		      br.close();
+			//int count = stockBean.getTotalCount();
 			bwObj.write(stockBean.getSymbol()+","+stockBean.getLastSale()
 					+","+stockBean.getDilutedEPS()+","+stockBean.getOperatingMargin()
 					+","+stockBean.getReturnOnAssets()+","+stockBean.getReturnOnEquity()
 					+","+stockBean.getRevenuePerShare());
 			bwObj.newLine();  
-			/*System.out.println(stockBean.getSymbol()+","+stockBean.getLastSale()
+			
+			System.out.println("[ "+( stockBean.getTotalCount())+" ]"+stockBean.getSymbol()+","+stockBean.getLastSale()
 					+","+stockBean.getDilutedEPS()+","+stockBean.getOperatingMargin()
 					+","+stockBean.getReturnOnAssets()+","+stockBean.getReturnOnEquity()
-					+","+stockBean.getRevenuePerShare());*/
+					+","+stockBean.getRevenuePerShare());
 			
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
+		} finally {
+
+		      if(connection != null) {
+		        connection.disconnect(); 
+		      }
+		    }
 		
 	}
 
@@ -62,6 +75,15 @@ public class AMUMStockDAO implements Runnable {
 				line = line.replace("</td><td class=\"yfnc_tabledata1\">", "");
 				line = line.replace("Diluted EPS (ttm):", "");
 				line = line.substring(0, line.indexOf("</td>"));
+				line = line.replace(",", "");
+				//divide EPS / 100
+				/*if(!line.equalsIgnoreCase("N/A")){
+					double dValue = Double.valueOf(line);
+					BigDecimal decimal1 = BigDecimal.valueOf(dValue);
+					BigDecimal decimal2 = new BigDecimal(100); 
+					BigDecimal total = decimal1.divide(decimal2, new MathContext(4));
+					line = String.valueOf(total);
+				}*/
 				stockBean.setDilutedEPS(line);
 			} else if (line
 					.contains("There is no Key Statistics data available")) {
@@ -73,29 +95,14 @@ public class AMUMStockDAO implements Runnable {
 
 	}
 
-/*	private String getKeyStatistics(String inputString, String startString, String endString) {
-
-		if(inputString.contains(startString)){
-			inputString = inputString.substring(inputString.indexOf(startString), inputString.lastIndexOf(endString));
-			inputString = inputString.replace("</td><td class=\"yfnc_tabledata1\">", "");
-			inputString = inputString.replace(startString,"");
-			inputString = inputString.substring(0,inputString.indexOf("</td>"));
-			return endString ;
-		}else if(inputString.contains("There is no Key Statistics data available")){
-			inputString = "No Data";
-			return ;
-		}
-	
-		return inputString;
-	}
-*/
-	private void findRevenuePerShare(String line) {
+private void findRevenuePerShare(String line) {
 		try {
 			if(line.contains("Revenue Per Share (ttm):")){
 	    		line = line.substring(line.indexOf("Revenue Per Share (ttm):"), line.lastIndexOf("Qtrly Revenue Growth (yoy):"));
 				line = line.replace("</td><td class=\"yfnc_tabledata1\">", "");
 				line = line.replace("Revenue Per Share (ttm):","");
 				line = line.substring(0,line.indexOf("</td>"));
+				line = line.replace(",", "");
 				stockBean.setRevenuePerShare(line);
 			}else if(line.contains("There is no Key Statistics data available")){
 				stockBean.setRevenuePerShare("No Data");
@@ -113,6 +120,7 @@ public class AMUMStockDAO implements Runnable {
 				line = line.replace("</td><td class=\"yfnc_tabledata1\">", "");
 				line = line.replace("Return on Equity (ttm):","");
 				line = line.substring(0,line.indexOf("</td>"));
+				line = line.replace(",", "");
 				stockBean.setReturnOnEquity(line);
 			}else if(line.contains("There is no Key Statistics data available")){
 				stockBean.setReturnOnEquity("No Data");
@@ -130,6 +138,7 @@ public class AMUMStockDAO implements Runnable {
 				line = line.replace("</td><td class=\"yfnc_tabledata1\">", "");
 				line = line.replace("Return on Assets (ttm):","");
 				line = line.substring(0,line.indexOf("</td>"));
+				line = line.replace(",", "");
 				stockBean.setReturnOnAssets(line);
 			}else if(line.contains("There is no Key Statistics data available")){
 				stockBean.setReturnOnAssets("No Data");
@@ -147,6 +156,7 @@ public class AMUMStockDAO implements Runnable {
 				line = line.replace("</td><td class=\"yfnc_tabledata1\">", "");
 				line = line.replace("Operating Margin (ttm):","");
 				line = line.substring(0,line.indexOf("</td>"));
+				line = line.replace(",", "");
 				stockBean.setOperatingMargin(line);
 			}else if(line.contains("There is no Key Statistics data available")){
 				stockBean.setOperatingMargin("No Data");
@@ -155,23 +165,4 @@ public class AMUMStockDAO implements Runnable {
 			e.printStackTrace();
 		}
 	}
-
-	/*private void findEPS(String line) {
-		try {
-			if(line.contains("Diluted EPS (ttm):")){
-				line = line.substring(line.indexOf("Diluted EPS (ttm):"), line.lastIndexOf("</td></tr><tr><td class=\"yfnc_tablehead1\" width=\"74%\">Qtrly Earnings Growth (yoy):</td>"));
-				line = line.replace("</td><td class=\"yfnc_tabledata1\">", "");
-				line = line.replace("Diluted EPS (ttm):","");
-				stockBean.setEps(line);
-			}else if(line.contains("There is no Key Statistics data available")){
-				stockBean.setEps("No Data");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}*/
-
-
-
 }
