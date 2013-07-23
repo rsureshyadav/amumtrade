3fbf098d6963c5ba5660bdf1822ebadd383fecbc
rsureshyadav@gmail.com <rsureshyadav@gmail.com@110281c2-ae3b-db2f-c1e3-5f523cf22f73>
2013-07-23 12:28:31 +0000

    EPSTTM changes

--- branches/AMUMTrade1.1/src/com/amumtrade/dao/EPSTTMServiceDao.java
+++ branches/AMUMTrade1.1/src/com/amumtrade/dao/EPSTTMServiceDao.java
@@ -24,19 +24,26 @@ public class EPSTTMServiceDao implements Runnable {
 			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=" + charset);
 			BufferedReader reader = null;
 			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			
 			for (String line; (line = reader.readLine()) != null;) {
-				if(line.contains("Diluted")){
-					line = line.substring(line.indexOf("Diluted EPS (ttm):"), line.lastIndexOf("</td></tr><tr><td class=\"yfnc_tablehead1\" width=\"74%\">Qtrly Earnings Growth (yoy):</td>"));
-					line = line.replace("</td><td class=\"yfnc_tabledata1\">", "");
-					stockBean.setEpsTtm(line);
-					System.out.println(stockBean.getSymbol()+"<>"+line);
-				}else{
-					stockBean.setEpsTtm("No Data");
-					System.out.println("No Data");
-				}
+				findEPSTTM(line);
+				
 	        }
+			System.out.println(stockBean.getSymbol()+","+stockBean.getEpsTtm());
 		} catch (Exception e) {
 			// TODO: handle exception
+		}
+		
+	}
+
+	private void findEPSTTM(String line) {
+		if(line.contains("Diluted")){
+			line = line.substring(line.indexOf("Diluted EPS (ttm):"), line.lastIndexOf("</td></tr><tr><td class=\"yfnc_tablehead1\" width=\"74%\">Qtrly Earnings Growth (yoy):</td>"));
+			line = line.replace("</td><td class=\"yfnc_tabledata1\">", "");
+			line = line.replace("Diluted EPS (ttm):","");
+			stockBean.setEpsTtm(line);
+		}else if(line.contains("There is no Key Statistics data available")){
+			stockBean.setEpsTtm("No Data");
 		}
 		
 	}
