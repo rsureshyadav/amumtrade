17dd23733669819812692407e41df621544691fb
rsureshyadav@gmail.com <rsureshyadav@gmail.com@110281c2-ae3b-db2f-c1e3-5f523cf22f73>
2013-07-23 12:22:36 +0000

    EPSTTM changes

---
+++ test/junit/com/amumtrade/handler/NasdaqStockTest.java
@@ -0,0 +1,30 @@
+package com.amumtrade.handler;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Test;
+
+import com.amumtrade.constant.StringConstant;
+
+public class NasdaqStockTest{
+	
+
+	@Test
+	public void fetchNasdaqStock() throws Exception{
+		double startRange = 0.8;
+		double endRange = 3;
+		StockHandler handler = new StockHandler( StringConstant.NASDAQ_NAME);
+		handler.execute(startRange, endRange, StringConstant.NASDAQ_URL,StringConstant.NASDAQ_INPUT_PATH+getTodayDate()+".csv", StringConstant.NASDAQ_OUTPUT_PATH);
+		}
+
+
+	private String getTodayDate() throws Exception {
+		String sDate;
+		SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
+		Date date = new Date();
+		sDate= sdf.format(date);
+		return sDate;
+	}
+
+}
