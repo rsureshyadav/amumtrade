6f3a8747b77f00a02066d4808c5f4654e36a9a48
rsureshyadav@gmail.com <rsureshyadav@gmail.com@110281c2-ae3b-db2f-c1e3-5f523cf22f73>
2013-12-20 05:21:04 +0000

    added 52wk high

--- src/com/amumtrade/helper/AMUMMoneyControlHelper.java
+++ src/com/amumtrade/helper/AMUMMoneyControlHelper.java
@@ -13,6 +13,7 @@ import sun.net.www.protocol.http.HttpURLConnection;
 public class AMUMMoneyControlHelper {
 
 	public Map<String, String> digest() throws IOException {
+		//Net Sales as per the latest Profit & Loss Account available.
 		String httpUrl="http://www.moneycontrol.com/stocks/top-companies-in-india/net-profit-nse.html";
 		String companyName = null;
 		String lastPrice = null;
@@ -22,6 +23,7 @@ public class AMUMMoneyControlHelper {
 	    HttpURLConnection connection = null;  
 	    StringBuffer strBuffer = new StringBuffer();
 	    Map<String, String> resultMap =   new  HashMap<String, String>();
+
 	    try {
 			  url = new URL(httpUrl);
 		      connection = (HttpURLConnection)url.openConnection();
@@ -54,21 +56,81 @@ public class AMUMMoneyControlHelper {
 							// To find the Change price
 							  change = line.substring(line.indexOf(">")+1, line.lastIndexOf("</td>")).trim();
 							  resetCount ++;
-							  System.out.println(companyName+","+lastPrice+","+change); 
+							  //System.out.println(companyName+","+lastPrice+","+change); 
 							  resultMap.put(lastPrice, companyName+","+lastPrice+","+change);
 						  }
 						 
 					  }
-					  if(line.contains("<td align=\"right\" class=\"brdrgtgry\"") && resetCount != 0){
-						  
-					  }
-
 				  }
+
 			  }
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return resultMap;
 		
+	}
+
+	public Map<String, String> fiftyTwoWeekAHighDigest() throws IOException{
+
+		//Net Sales as per the latest Profit & Loss Account available.
+		String httpUrl="http://www.moneycontrol.com/stocks/marketstats/highlows/index.php?sel_opt=highs&sel_index=B|A|A%20Group";
+		
+		String companyName = null;
+		String wkHigh = null;
+		String intraHigh = null;
+		String lastClose = null;
+
+
+		URL url = null;
+	    HttpURLConnection connection = null;  
+	    StringBuffer strBuffer = new StringBuffer();
+	    Map<String, String> fiftyTwoWeekHighMap =   new  HashMap<String, String>();
+
+	    try {
+			  url = new URL(httpUrl);
+		      connection = (HttpURLConnection)url.openConnection();
+		      connection.setReadTimeout(30000);
+		      InputStream is = connection.getInputStream();
+			  BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			  String wkLine;
+			  boolean isCapture = false;
+			  int resetCount = 0; 
+			  while ((wkLine = br.readLine()) != null) {
+				  if(wkLine.contains("Company</th>")){
+					  isCapture = true;
+				  }
+				  if(wkLine.contains("function validation()")){
+					  isCapture = false;
+				  }
+				  if(isCapture){
+					  System.out.println(wkLine);
+					  if(wkLine.contains("<td align=\"left\" class=\"brdrgtgry\">")){
+						  companyName = wkLine.substring(wkLine.indexOf("<b>")+3, wkLine.lastIndexOf("</b>")).trim();
+						  System.out.println(companyName);
+					  }
+					  if(wkLine.contains("<td width=\"19%\" align=\"right\" class=\"brdrgtgry")){ 
+						  wkHigh = wkLine.substring(wkLine.indexOf("brdrgtgry")+11, wkLine.lastIndexOf("</td>")).trim();
+						  System.out.println(wkHigh);
+
+						  }
+					  if(wkLine.contains("<td width=\"16%\" align=\"right\" class=\"brdrgtgry")){ 
+						  intraHigh = wkLine.substring(wkLine.indexOf("brdrgtgry")+12, wkLine.lastIndexOf("</td>")).trim();
+						  System.out.println(intraHigh);
+
+						  }
+					  if(wkLine.contains("<td width=\"14%\" align=\"right\" class=\"brdrgtgry")){ 
+						  lastClose = wkLine.substring(wkLine.indexOf("brdrgtgry")+12, wkLine.lastIndexOf("</td>")).trim();
+						  System.out.println(lastClose);
+
+						  }
+					  }
+			  }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	
 	}
 }
