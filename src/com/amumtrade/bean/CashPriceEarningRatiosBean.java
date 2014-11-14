6c0cd224696d133d7e5a15884fed9dce4b6cb5ce
rsureshyadav@gmail.com <rsureshyadav@gmail.com@110281c2-ae3b-db2f-c1e3-5f523cf22f73>
2014-11-14 13:58:38 +0000

    Added MarketStat

---
+++ src/com/amumtrade/bean/CashPriceEarningRatiosBean.java
@@ -0,0 +1,21 @@
+package com.amumtrade.bean;
+
+public class CashPriceEarningRatiosBean {
+	private String name;
+	private String lastPrice;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastPrice() {
+		return lastPrice;
+	}
+	public void setLastPrice(String lastPrice) {
+		this.lastPrice = lastPrice;
+	}
+
+	
+}
---
+++ src/com/amumtrade/marketstat/PriceEarningRatios.java
@@ -0,0 +1,5 @@
+package com.amumtrade.marketstat;
+
+public class PriceEarningRatios {
+//http://www.moneycontrol.com/stocks/marketinfo/pe/bse/homebody.php?indcode=0&sortcode=0
+}
