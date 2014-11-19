44d14efbc70258d1a7f77084d79dcb6203d39cfd
rsureshyadav@gmail.com <rsureshyadav@gmail.com@110281c2-ae3b-db2f-c1e3-5f523cf22f73>
2014-11-19 01:42:44 +0000

    added timmer

--- src/com/amumtrade/util/RunStockTimer.java
+++ src/com/amumtrade/util/RunStockTimer.java
@@ -1,46 +1,22 @@
 package com.amumtrade.util;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 public class RunStockTimer {
 
 	public static void main(String str[]){
-		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		Calendar cal = Calendar.getInstance();
-		System.out.println(dateFormat.format(cal.getTime()));
-
+	
 		try {
-		    String string1 = "07:05:00";
-		    Date time1 = new SimpleDateFormat("HH:mm:ss").parse(string1);
-		    Calendar calendar1 = Calendar.getInstance();
-		    calendar1.setTime(time1);
-
-		    String string2 = "07:07:00";
-		    Date time2 = new SimpleDateFormat("HH:mm:ss").parse(string2);
-		    Calendar calendar2 = Calendar.getInstance();
-		    calendar2.setTime(time2);
-		    calendar2.add(Calendar.DATE, 1);
-
-		    /*String someRandomTime = "01:00:00";
-		    Date d = new SimpleDateFormat("HH:mm:ss").parse(someRandomTime);
-		    Calendar calendar3 = Calendar.getInstance();
-		    calendar3.setTime(d);
-		    calendar3.add(Calendar.DATE, 1);*/
-
-		   /* Date x = calendar3.getTime();
-		    if (x.after(calendar1.getTime()) && x.before(calendar2.getTime())) {
-		        //checkes whether the current time is between 14:49:00 and 20:11:13.
-		        System.out.println(true);
-		    }*/
-		    
-		    Date x = calendar2.getTime();
-		    if (x.after(calendar1.getTime()) && x.before(calendar2.getTime())) {
-		        //checkes whether the current time is between 14:49:00 and 20:11:13.
-		        System.out.println("loop execute");
-		    }
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+			Date before = sdf.parse("19/11/2014 07:00");
+			Date after = sdf.parse("19/11/2014 07:30");
+			Date toCheck = sdf.parse("19/11/2014 06:09");
+			//is toCheck between the two?
+			boolean isAvailable = (before.getTime() < toCheck.getTime()) && after.getTime() > toCheck.getTime();
+			if(isAvailable){
+				System.out.println("Hellow World!!!!");
+			}
 		} catch (Exception e) {
 		    e.printStackTrace();
 		}
